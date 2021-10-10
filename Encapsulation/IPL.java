cd69c7abd58612543b33eb6b2a43a16ee4e711ea
Priya-nc <62901413+Priya-nc@users.noreply.github.com>
2021-10-10 21:29:52 +0530

    Add files via upload

--- Encapsulation/IPL.java
+++ Encapsulation/IPL.java
@@ -1,36 +1,79 @@
 import java.util.Scanner;
 class IPL{
+	static Scanner scan=new Scanner(System.in);
+	IPLTeams []ipl1=new IPLTeams[0];
+	IPLTeams ipl;
+	int n;
+	static IPL ip;
+	String teamName;
+	byte numTeamMemeber;
+	byte numBatsMan;
+	byte numBollers;
 public static void main(String []arg){
-	Scanner scan=new Scanner(System.in);
-	System.out.println("Enter number of Teams ");
-	int n=scan.nextInt();
-	System.out.println("Enter Team details");
-	IPLTeams []ipl1=new IPLTeams[n];
+	
+	ip=new IPL();
+	String more;
+	int n;
+		
+		do{
+		System.out.println("To give team details type give, to display type display");
+		String inp=scan.nextLine();
+		if(inp.equals("give")){
+		    System.out.println("Enter Team details");
+			ip.give();}
+		else if (inp.equals("display")){
+			
+			ip.display();}
+		else
+			System.out.println("enter valid input");
+		
+		System.out.println("you want to give more details type more");
+		more=scan.nextLine();
+		
+      }	while(more.equals("more"));
+}
+
+	 void give(){
+		System.out.println("Enter number of Teams ");
+		ip.n=scan.nextInt();
+		ipl1=new IPLTeams[n];
 	for(int i=0;i<ipl1.length;i++){
+		
 		scan.nextLine();
 		System.out.println("Enter Team Name ");
-		String teamName=scan.nextLine();
+		ip.teamName=scan.nextLine();
+		//String teamName1=ip.teamName;
 		System.out.println("Enter number of Team members ");
-		byte numTeamMemeber=scan.nextByte();
+		ip.numTeamMemeber=scan.nextByte();
+		//byte numTeamMemeber1
 		System.out.println("Enter the number of batsman in Team ");
-		byte numBatsMan=scan.nextByte();
+		ip.numBatsMan=scan.nextByte();
 		System.out.println("Enter the numeber of bollers in Team ");
-		byte numBollers=scan.nextByte();
+		ip.numBollers=scan.nextByte();
+		scan.nextLine();
 		
-		IPLTeams ipl=new IPLTeams();
+		
+		ipl=new IPLTeams();
 		ipl.setTeamName(teamName);
 		ipl.setNumTeamMemeber(numTeamMemeber);
 		ipl.setNumBatsMan(numBatsMan);
 		ipl.setNumBollers(numBollers);
 		
 		ipl1[i]=ipl;
+		
+	  }
+	  
 	}
-	for(int i=0;i<ipl1.length;i++){
+
+	void display(){
+		for(int i=0;i<ipl1.length;i++){
 		IPLTeams ipl = ipl1[i];
 		System.out.println(ipl.getTeamName()+"\t"+ipl.getNumTeamMemeber()+"\t"+ipl.getnumBatsMan()+"\t"+ipl.getNumBollers());
+	  }
 	}
 	
-}
+
+
 }
 
 class IPLTeams{
