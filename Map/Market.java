43cd169cbeb192032a3f55a519d82289e8094bbe
Priya-nc <62901413+Priya-nc@users.noreply.github.com>
2021-10-20 01:02:03 +0530

    Add files via upload

---
+++ Map/Market.java
@@ -0,0 +1,21 @@
+package com.wolken.market.map;
+
+import java.util.*;
+
+public class Market {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         HashMap hmap=new HashMap();
+         hmap.put("a", "p");
+        System.out.println( hmap);
+        TreeMap tmap=new TreeMap();
+        tmap.put("a", "p");
+        System.out.println( tmap);
+        LinkedHashMap lmap=new LinkedHashMap();
+        lmap.put("a", "p");
+        System.out.println( lmap);
+        
+	}
+
+}
