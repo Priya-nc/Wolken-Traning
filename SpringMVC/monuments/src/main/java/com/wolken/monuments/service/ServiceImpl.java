b965b0f4f2a480b02cf6c775f9f8a725810c2961
Priya-nc <62901413+Priya-nc@users.noreply.github.com>
2021-11-09 10:35:03 +0530

    Add files via upload

---
+++ SpringMVC/monuments/src/main/java/com/wolken/monuments/service/ServiceImpl.java
@@ -0,0 +1,10 @@
+package com.wolken.monuments.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ServiceImpl {
+	public void printHello() {
+		System.out.println("Hello service");
+	}
+}
